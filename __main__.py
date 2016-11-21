30802dba220bf6232dffd8d844d9759a8f6df115
Will Mischlewski <will.mischlewski@lonsec.com.au>
2016-11-21 13:49:03 +1100

    Also save when listing tasks

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ app = ChaidoApp()
 app.load(".chaido")
 if len(sys.argv) <= 2:
     print(listToDos(app, []))
+    app.save(".chaido")
 else:
     cleanedArguments = sys.argv[2:]
     if sys.argv[1] not in commands:
@@ -14,9 +15,9 @@ else:
         exit()
     try:
         print(commands[sys.argv[1]](app, cleanedArguments))
-        app.save(".chaido")
         with open(".chaido.log", 'a') as f:
             for logMessage in app.getLogMessages():
                 f.write("|".join([datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), logMessage['command'], logMessage['message']]) + "\n")
+        app.save(".chaido")
     except ChaidoError as error:
         print("Error: " + error.message)
--- chaido.py
+++ chaido.py
@@ -14,7 +14,6 @@ def addNewTodo(app, arguments):
     return "OK"
 
 def removeTodo(app, arguments):
-    arguments.sort(reverse=True)
     while len(arguments) > 0:
         app.removeTodo(arguments.pop(0))
     return "OK"
